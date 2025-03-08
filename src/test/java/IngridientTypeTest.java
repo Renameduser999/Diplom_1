2fdae4c744a5f656713826adcf846e2f74dc3468
Dmitriy Chernobrov <dimon.perm.nl@gmail.com>
2025-03-08 16:42:07 +0500

    final commit 1

--- src/test/java/IngridientTypeTest.java
+++ src/test/java/IngridientTypeTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertNotNull;
+
+
 
 public class IngridientTypeTest {
+
+    @Test
+    public void SAUCETest(){
+        assertNotNull("Опаньки, где-то ошибка", IngredientType.SAUCE);
+    }
+
+    @Test
+    public void FILLINGTest() {
+        assertNotNull("Опаньки, где-то ошибка", IngredientType.FILLING);
+    }
 }
